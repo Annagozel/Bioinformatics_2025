d92709af215197f8d56e669af9aca4cee0ee45cb
Annagozel <114587143+Annagozel@users.noreply.github.com>
2025-04-25 14:39:07 +0300

    Create run_qc.sh

---
+++ hw_file_9/run_qc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fastqc ERR14230569_1.fastq ERR14230569_2.fastq -o fastqc_results
+fastqc ERR14230570_1.fastq ERR14230570_2.fastq -o fastqc_results
+fastqc ERR14230571_1.fastq ERR14230571_2.fastq -o fastqc_results
+fastqc ERR14230572_1.fastq ERR14230572_2.fastq -o fastqc_results
